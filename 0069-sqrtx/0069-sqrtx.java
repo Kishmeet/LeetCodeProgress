2c9ae12c0dd8cd1ebb126bc3f0502668b3c4c34d
Kishmeet Singh Khurana <kishmeetsinghk@gmail.com>
2026-07-18 01:19:58 +0530

    Time: 1 ms (97.56%), Space: 42.7 MB (23.58%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low = 0, high = x;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // if(mid*mid <=x) overflow
+            if ((long)mid * mid <= x )
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return high;
+    }
+}
