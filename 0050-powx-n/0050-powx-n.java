1b566446f326ea5cdb5b75f9ce58b47c118a2ac1
Kishmeet Singh Khurana <kishmeetsinghk@gmail.com>
2026-07-18 01:07:31 +0530

    Time: 0 ms (100%), Space: 48.2 MB (34.96%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double myPow(double x, long n) {
+        if (n == 0)
+            return 1;
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double res = myPow(x, n / 2);
+        if (n % 2 == 0)
+            return res * res;
+        else
+            return res * res * x;
+    }
+}
